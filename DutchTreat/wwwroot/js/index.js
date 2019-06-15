f6f1d577c944c4692d556190473b8ed5798d8cd7
meghannhumanfactors <31716705+meghannhumanfactors@users.noreply.github.com>
2019-06-15 16:39:24 -0700

    added json config... look for it under node modules... added dependencies to the package json file then added jquery ^3.3.1

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,1 +1,16 @@
-﻿console.log("Hello Purlasight");
+﻿var x = 0;
+var s = "";
+//alert("Hello Pluralsight");
+//console.log("Hello Purlasight");
+
+//Hides the form
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+});
+
+var productInfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children;
