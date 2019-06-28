e59bedd52c5da1aecc401320a0b44ed4d4ab193b
meghannhumanfactors <31716705+meghannhumanfactors@users.noreply.github.com>
2019-06-28 00:26:01 -0700

    fixed validation

--- DutchTreat/ViewModels/ContactViewModel.cs
+++ DutchTreat/ViewModels/ContactViewModel.cs
@@ -5,18 +5,19 @@ namespace DutchTreat.ViewModels
     public class ContactViewModel
     {
         [Required]
-        [MinLength(2)]
+        [MinLength(2, ErrorMessage = "Please enter a name.")]
         public string UserName { get; set; }
 
         [Required]
-        [EmailAddress]
+        [EmailAddress(ErrorMessage = "Pleae add a valid email address.")]
         public string UserEmail { get; set; }
 
         [Required]
+        [MaxLength(50, ErrorMessage = "Please add a subject to your message.")]
         public string Subject { get; set; }
 
         [Required]
-        [MaxLength(250)]
+        [MaxLength(250, ErrorMessage = "Your message is to long please reduce it.")]
         public string UserMessage { get; set; }
 
     }
