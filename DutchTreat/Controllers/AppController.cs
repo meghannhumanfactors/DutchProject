4237a7ae95b9f36138d7460915620fe43e16c5bd
meghannhumanfactors <31716705+meghannhumanfactors@users.noreply.github.com>
2019-07-07 17:49:02 -0700

    .

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -1,65 +1,61 @@
 ﻿using DutchTreat.Services;
 using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using System;
 
 
 
 namespace DutchTreat.Controllers
 {
     public class AppController : Controller
-         private readonly IMailService _mailService;
-
-    public AppController(IMailService mailService)
     {
-        _mailService = mailService;
-    }
+        private readonly IMailService _mailService;
 
-
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-    public IActionResult Contact()
-    {
-        return View();
-    }
-
-    [HttpPost("contact")]
-    public IActionResult Contact(ContactViewModel model)
-    {
-        if (ModelState.IsValid)
+        public AppController(IMailService mailService)
         {
-            //Send the email
-            _mailService.SendMail("shawn@wildermuth.com", model.Subject, $"From: {model.UserName} - {model.UserEmail}, Message: {model.UserMessage}");
-            ViewBag.UserMessage = "Mail Sent";
-            ModelState.Clear();
+            _mailService = mailService;
         }
-        //else
-        //{
-        //    //Show the errors
-        //}
 
 
-        ViewBag.Title = "Contact Us";
 
-        return View();
-    }
-
-    internal class _mailService
-    {
-        internal static void SendMail(string v1, string subject, string v2)
+        public IActionResult Index()
         {
-            throw new NotImplementedException();
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Send the email
+                _mailService.SendMessage("shawn@wildermuth.com", model.Subject, $"From: {model.UserName} - {model.UserEmail}, Message: {model.UserMessage}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+            //else
+            //{
+            //    //Show the errors
+            //}
+
+
+            ViewBag.Title = "Contact Us";
+
+            return View();
+        }
+
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
         }
     }
-
-    public IActionResult About()
-    {
-        ViewBag.Title = "About Us";
-
-        return View();
-    }
-}
 }
